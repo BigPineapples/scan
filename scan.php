7e4ceb1921d4fb551f9534292121826fa6bdd1ec
hhq <137383816@qq.com>
2020-09-01 14:52:47 +0800

    创建

---
+++ scan.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+
+
+$client = new \GuzzleHttp\Client();
+
+Hhq\Index\index::a();
+
+$csv = \League\Csv\Reader::createFromPath($argv[1], 'r');
+$csv->getRecords();
+
+foreach ($csv as $csvRow) {
+    try {
+        $httpResponse = $client->options($csvRow[0]);
+
+        if ($httpResponse->getStatusCode() >= 400) {
+            throw new \Exception();
+        }
+    } catch (\Exception $e) {
+        echo $csvRow[0] . PHP_EOL;
+    }
+}
---
+++ src/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hhq\Index;
+
+class index
+{
+    static function a()
+    {
+        echo 1;
+    }
+}
